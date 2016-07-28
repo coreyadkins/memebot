38b0756d13d7cea2dc08c4156b1decbaf8c197f9
Katie Nichols <Kaylotura@gmail.com>
2016-07-28 11:40:08 -0700

    added docstrings and broke apart some functions. included repr and eq for Markov object

--- markov_chain.py
+++ markov_chain.py
@@ -8,11 +8,18 @@ class Markov(object):
     """
 
     def __init__(self, open_file):
-        """Initiates the Markov chain.
+        r"""Initiates the Markov chain.
 
-        >>> a = Markov()
-        >>> a
-        'words I guess'
+        >>> a = Markov(open('test.txt'))
+        >>> b = list(a.cache.items())
+        >>> sorted(b)
+        [(('This', 'is'), ['a']), (('is', 'a'), ['test.'])]
+        >>> a = Markov(open('test.txt'))
+        >>> a.words
+        ['This', 'is', 'a', 'test.']
+        >>> a = Markov(open('test.txt'))
+        >>> a.word_size
+        4
         """
         self.cache = {}
         self.open_file = open_file
@@ -23,36 +30,41 @@ class Markov(object):
     def __repr__(self):
         """Represents the Markov chain...
 
-        >>> a = Markov()
-        >>> a
-        'words I guess'        """
-        return 'Markov({!r}, {!r}, {!r}, {!r}, {!r})'.format(
-            self.cache, self.open_file, self.words, self.word_size,
-            self.database())
+        >>> a = Markov(open('test.txt'))
+        >>> b = list(a.cache.items())
+        >>> sorted(b)
+        [(('This', 'is'), ['a']), (('is', 'a'), ['test.'])]
+        >>> a = Markov(open('test.txt'))
+        >>> a.words
+        ['This', 'is', 'a', 'test.']
+        >>> a = Markov(open('test.txt'))
+        >>> a.word_size
+        4
+        """
+        return 'Markov({!r}, {!r}, {!r})'.format(self.cache, self.words,
+                                                 self.word_size)
 
     def __eq__(self, other):
-        """Defines equality in Markov Objects.
+        r"""Defines equality in Markov Objects.
 
-        >>> a = Markov()
-        >>> a.word_size = 3
-        >>> b = Markov()
-        >>> a.word_size = 3
+        >>> a = Markov(open('test.txt'))
+        >>> b = Markov(open('test.txt'))
         >>> a == b
         True
-        >>> a = Markov()
-        >>> a.word_size = 3
-        >>> b = Markov()
-        >>> a.word_size = 5
+        >>> a = Markov(open('test.txt'))
+        >>> b = Markov(open('not_test.txt'))
         >>> a == b
         False
         """
-        return (
-            self.cache == other.cache and self.open_file == other.open_file and
-            self.words == other.words and self.word_size == other.word_size and
-            self.database() == other.database())
+        return (self.cache == other.cache and self.words == other.words and
+                self.word_size == other.word_size)
 
     def file_to_words(self):
         """Opens a text file and splits all the words into a list.
+
+        >>> a = Markov(open('test.txt'))
+        >>> a.file_to_words()
+        ['This', 'is', 'a', 'test.']
         """
         self.open_file.seek(0)
         data = self.open_file.read()
@@ -61,6 +73,11 @@ class Markov(object):
 
     def triples(self):
         """Generates triples (3-grams) from the given data string.
+
+        >>> a = Markov(open('test.txt'))
+        >>> b = a.triples()
+        >>> list(b)
+        [('This', 'is', 'a'), ('is', 'a', 'test.')]
         """
 
         if len(self.words) < 3:
@@ -70,6 +87,8 @@ class Markov(object):
             yield (self.words[i], self.words[i + 1], self.words[i + 2])
 
     def database(self):
+        """Creates a data base of words from triples and number of words.
+        """
         for w1, w2, w3 in self.triples():
             key = (w1, w2)
             if key in self.cache:
@@ -78,6 +97,7 @@ class Markov(object):
                 self.cache[key] = [w3]
 
     def generate_top_text(self, size=(random.randrange(1, 7))):
+        """Generates a phrase from the sample text anywhere between 1 and 6 tokens long."""
         seed = random.randint(0, self.word_size - 3)
         seed_word, next_word = self.words[seed], self.words[seed + 1]
         w1, w2 = seed_word, next_word
@@ -89,6 +109,7 @@ class Markov(object):
         return ' '.join(gen_words)
 
     def generate_bottom_text(self, size=(random.randrange(1, 11))):
+        """Generates a phrase from the sample text anywhere between 1 and 10 tokens long."""
         seed = random.randint(0, self.word_size - 3)
         seed_word, next_word = self.words[seed], self.words[seed + 1]
         w1, w2 = seed_word, next_word
--- text_generator.py
+++ text_generator.py
@@ -3,17 +3,28 @@
 import markov_chain
 
 
-def output(markov):
-    top = (markov.generate_top_text()).upper()
-    bottom = (markov.generate_bottom_text()).upper()
+def get_top_line(markov):
+    """Generates a single string using Markov chains of 1 to 6 tokens"""
+    return (markov.generate_top_text()).upper()
+
+
+def get_bottom_line(markov):
+    """Generates a single string using Markov chains of 1 to 11 tokens"""
+    return (markov.generate_bottom_text()).upper()
+
+
+def output(top, bottom, markov):
+    """Returns a list of two short strings top first, bottom second."""
     meme_words = [top, bottom]
-    print(meme_words)
+    return meme_words
 
 
 def main():
     file = open('sample.txt')
     markov = markov_chain.Markov(file)
-    output(markov)
+    top_line = get_top_line(markov)
+    bottom_line = get_bottom_line(markov)
+    return output(top_line, bottom_line, markov)
 
 
 if __name__ == '__main__':
