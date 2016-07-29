2970a27839db8b44e78f59974d46a58016d4d703
Andrew Champion <awchampion@gmail.com>
2016-07-29 11:35:13 -0700

    Corpus Updates
    Top 500 maymays (memes) downloaded for corpus text and source images

--- corpus.py
+++ corpus.py
@@ -2,22 +2,25 @@
 
 
 from mgapi.handler import MemeGeneratorApiHandler
+import os
+import requests
 
 
 def main():
     """ main """
-    username = input('dankm')
-    username = 'dankm' + username + 'ter420'
+    INPUT_DIR = 'input/'
 
+    # username = input('dankm')
+    username = '{}{}{}{}{}{}{}'.format(' da', 'nk', 'ma', 'st', 'er', '42', '0 ').strip()
     builder = MemeGeneratorApiHandler(username, 'pdxc0d3gu!ld')
-    top_ids = builder.get_popular_meme_ids(500)
-    top_memes = [builder.get_meme_detail(x) for x in top_ids]
+    top_meme_ids = builder.get_popular_meme_ids(500)
+    top_memes = [builder.get_meme_detail(x) for x in top_meme_ids]
 
-<<<<<<< HEAD
-    write_corpus(top_memes, 'input/mgapi.txt')
-=======
-    write_corpus(top_memes, 'input/corpus.txt')
->>>>>>> f3b1d3aec3ce6f62a9379b3da31a6a8ed1c623bc
+    top_gen_ids = set([(meme['generatorID'], meme['urlName']) for meme in top_memes])
+    top_gens = [builder.get_generator_detail(id, name) for id, name in top_gen_ids]
+
+    write_corpus(top_memes, INPUT_DIR + 'corpus.txt')
+    save_generators(top_gens, INPUT_DIR)
 
 
 def write_corpus(meme_list, path):
@@ -25,7 +28,30 @@ def write_corpus(meme_list, path):
     with open(path, 'w') as file_handle:
         for meme in meme_list:
             text = [meme[key] for key in meme if 'text' in key and meme[key] is not None]
-            file_handle.write(' '.join(text).strip() + '\n')
+            file_handle.write('\n'.join(text).strip() + '\n')
+
+
+def save_generators(generators, path, prune=False):
+    """ Saves necessary generators, deletes unnecessary """
+    existing_names = [file[:-4] for file in os.listdir(path) if file.endswith('.jpg')]
+    preserve = []
+
+    for keep in generators:
+        preserve.append(keep['urlName'])
+
+        if keep['urlName'] not in existing_names:
+            # download
+            rq = requests.get(keep['imageUrl'])
+            print('Downloading {} from {}'.format(keep['urlName'], keep['imageUrl']))
+            with open(path + keep['urlName'] + '.jpg', 'wb') as file:
+                file.write(rq.content)
+        else:
+            print('FOUND: ' + keep['urlName'])
+
+    for kill in [path + file + '.jpg' for file in existing_names if file not in preserve]:
+        print('Delete{}: {}'.format(' (not really)' if not prune else '', kill))
+        if prune:
+            os.unlink(kill)
 
 
 if __name__ == '__main__':
--- mgapi/handler.py
+++ mgapi/handler.py
@@ -21,16 +21,18 @@ class MemeGeneratorApiHandler:
         url_params = {} if len(params) == 0 else params
         rq = requests.get(url_str, url_params)
         data_text = rq.text
-        data_json = json.loads(data_text)['result']
+
+        try:
+            data_json = json.loads(data_text)['result']
+        except KeyError:
+            raise ValueError('Invalid data at ' + rq.url)
+
         return data_json
 
-    def _get_ids_from_json(self, meme_list):
-        """ Parse out just the IDs from a list of memes
-
-        :return list(int: generator_id)
-        """
-        ids = [int(maymay['instanceID']) for maymay in meme_list]
-        return ids
+    def _add_login_to_params(self, addl_params=None):
+        params = {} if addl_params is None else addl_params
+        params.update({'username': self._username, 'password': self._password})
+        return params
 
     def get_popular_meme_ids(self, total, days=None):
         """ Returns list of popular generators
@@ -41,22 +43,16 @@ class MemeGeneratorApiHandler:
         max_results = 24
 
         for i in range(0, total, max_results):
-            params = {
+            params = self._add_login_to_params({
                 'pageIndex': i // max_results,
-                'pageSize': min(max_results, total - i),
-                'username': self._username,
-                'password': self._password
-            }
+                'pageSize': min(max_results, total - i)
+            })
 
             if days is not None:
                 params['days'] = days
 
-            data_json = self._load_action(
-                'Instances_Select_ByPopular',
-                params
-            )
-
-            top_maymays += self._get_ids_from_json(data_json)
+            data_json = self._load_action('Instances_Select_ByPopular', params)
+            top_maymays += [int(meme['instanceID']) for meme in data_json]
 
         return top_maymays
 
@@ -65,24 +61,29 @@ class MemeGeneratorApiHandler:
 
         :return list(int: generator_id)
         """
-        data_json = self._load_action('Generators_Select_ByTrending')
-        trending_maymays = self._get_ids_from_json(data_json)
-        return trending_maymays
+        # TODO: implement?
+        pass
+        # data_json = self._load_action('Generators_Select_ByTrending')
+        # trending_maymays = self._get_ids_from_json(data_json)
+        # return trending_maymays
 
     def get_meme_detail(self, meme_id):
-        """ Get details of meme of generatorID: meme_id
+        """ Get details of meme instanceID
 
         :return JSON: detail
         """
-        maymay_detail = self._load_action(
-            'Instance_Select',
-            {
-                'username': self._username,
-                'password': self._password,
-                'instanceID': meme_id
-            }
-        )
+        params = self._add_login_to_params({'instanceID': meme_id})
+        maymay_detail = self._load_action('Instance_Select', params)
 
         # that's a nice maymay.
         # [(8)D
         return maymay_detail
+
+    def get_generator_detail(self, generator_id, generator_name):
+        """ Get generator detail for meme """
+        params = self._add_login_to_params({
+            'generatorID': generator_id,
+            'urlName': generator_name,
+        })
+        detail = self._load_action('Generator_Select_ByUrlNameOrGeneratorID', params)
+        return detail
