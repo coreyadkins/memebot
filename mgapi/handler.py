90a91f9a4674590894c74c5b110a64ddf15a8ee5
Andrew Champion <awchampion@gmail.com>
2016-07-29 15:22:53 -0700

    Changes Per Feedback
    Additional module documentation

--- mgapi/handler.py
+++ mgapi/handler.py
@@ -1,6 +1,12 @@
-""" Corpus Fetcher
+""" memegenerator.net API handler
 
- Loads corpus data from MemeGenerator.net API
+DEFINITIONS:
+* generator: the base image with no text (hence memegenerator)
+* instance: the combination of a generator with text, i.e.
+        Most-Interesting-Man.webm
+        I don't always write documentation
+        But when I do, it's
+* maymay: memes for people who don't get memes
  """
 
 import requests
@@ -11,12 +17,16 @@ class MemeGeneratorApiHandler:
     """ Fetches mgapi data via API """
     def __init__(self, username, password):
         """ initializes l/p """
-
         self._username = username
         self._password = password
 
     def _load_action(self, action, params={}):
-        """ Wrapper for API requests """
+        """ Wrapper for API requests
+
+        >>> h = MemeGeneratorApiHandler('', '')
+        >>> h._load_action('Instance_Select', {'instanceID': 9173564})['text0']
+        'reading socially awkward penguin memes'
+        """
         url_str = 'http://{}/{}'.format('version1.api.memegenerator.net', action)
         url_params = {} if len(params) == 0 else params
         rq = requests.get(url_str, url_params)
@@ -30,14 +40,22 @@ class MemeGeneratorApiHandler:
         return data_json
 
     def _add_login_to_params(self, addl_params=None):
+        """ Add username & password to parameters
+
+        Some API calls require a login. This appends that info to the dict
+
+        >>> h = MemeGeneratorApiHandler('user', 'pass')
+        >>> sorted(list(h._add_login_to_params({'zz': 'top'}).items()))
+        [('password', 'pass'), ('username', 'user'), ('zz', 'top')]
+        """
         params = {} if addl_params is None else addl_params
         params.update({'username': self._username, 'password': self._password})
         return params
 
     def get_popular_meme_ids(self, total, days=None):
-        """ Returns list of popular generators
+        """ Returns list of popular meme instances
 
-        :return list(int: generator_id)
+        :return list(int: instance)
         """
         top_maymays = []
         max_results = 24
@@ -56,21 +74,13 @@ class MemeGeneratorApiHandler:
 
         return top_maymays
 
-    def get_trending_meme_ids(self):
-        """ Returns list of trending generators
-
-        :return list(int: generator_id)
-        """
-        # TODO: implement?
-        pass
-        # data_json = self._load_action('Generators_Select_ByTrending')
-        # trending_maymays = self._get_ids_from_json(data_json)
-        # return trending_maymays
-
     def get_meme_detail(self, meme_id):
         """ Get details of meme instanceID
 
         :return JSON: detail
+        >>> h = MemeGeneratorApiHandler('', '')
+        >>> h.get_meme_detail(9173564)['text1']
+        'relating to all of them'
         """
         params = self._add_login_to_params({'instanceID': meme_id})
         maymay_detail = self._load_action('Instance_Select', params)
@@ -80,7 +90,12 @@ class MemeGeneratorApiHandler:
         return maymay_detail
 
     def get_generator_detail(self, generator_id, generator_name):
-        """ Get generator detail for meme """
+        """ Get details for a specific generator
+
+        >>> h = MemeGeneratorApiHandler('', '')
+        >>> h.get_generator_detail(45, 'Insanity-Wolf')['displayName']
+        'Insanity Wolf'
+        """
         params = self._add_login_to_params({
             'generatorID': generator_id,
             'urlName': generator_name,
