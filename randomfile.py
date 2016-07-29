88d9c7706a2a74b3131e996b7a2e29a9fa4b33db
Andrew Champion <awchampion@gmail.com>
2016-07-29 14:55:59 -0700

    Changes, per feedback

--- randomfile.py
+++ randomfile.py
@@ -3,33 +3,20 @@
 from os.path import splitext, join
 import os
 import random
+from tempfile import mkstemp
 
 
-def get_unique_str(str_length=12):
-    """ Generate a random alphanumeric string (36^str_length permutations)
-
-    :return str: unique_string
-    """
-    VALID_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!'
-    random.seed()
-
-    unique = ''
-    for i in range(str_length):
-        idx = random.randrange(0, len(VALID_CHARS))
-        unique += VALID_CHARS[idx]
-
-    return unique
-
-
-def get_random_output_path(filename, dir = 'output'):
+def get_random_output_path(filename, dirname='output'):
     """ Appends a random string to a filename while preserving the extension
 
-    :return str: directory/basename.1234567890Ab.webm
+    :return str: directory/basename.r4Nd0Ms7r!n9.webm
+    >>> '/basename.' in get_random_output_path('basename.webm')
+    True
     """
     basename, ext = splitext(filename)
-    rando = get_unique_str()
-
-    return join(dir, '{}/{}.{}{}'.format(dir, basename, rando, ext))
+    handle, path = mkstemp(ext, basename + '.', dirname, '')
+    os.unlink(path)
+    return path
 
 
 def get_random_input_file(dir='input', extensions='.jpg'):
