a22d05671b0a0c175a8417aff7050aec7819ba1f
Katie Nichols <Kaylotura@gmail.com>
2016-07-29 12:29:48 -0700

    Getting hang of GUI manipulation, should have pretty GUI soon.

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,25 +1,50 @@
-import tkinter
+"""Opens a window and displays two random memes, user clicks meme and vote is saved,
+then window loops with two more random memes.
 
-window = tkinter.Tk()
-frame = tkinter.Frame(window)
-window.geometry('900x600')
-frame.pack()
-
-redbutton = tkinter.Button(frame, text="Green meme is so Dank!", fg="green")
-redbutton.pack(side=tkinter.LEFT)
-
-bluebutton = tkinter.Button(frame, text="Blue meme is so Dank!!", fg="blue")
-bluebutton.pack(side=tkinter.RIGHT)
-
-img_1 = "This will totally be an image! Go green meme!"
-image_1 = tkinter.Label(window, text=img_1)
-image_1.pack(side="left", fill="both", expand="yes")
-
-img_2 = "This will totally be an image! Go blue meme!"
-image_2 = tkinter.Label(window, text=img_2)
-image_2.pack(side="right", fill="both", expand="yes")
+Currently, just opens a window and displays two static memes and vote clicks do nothing. No loops as of yet.
 
 
+Future goal, click button to open alternate window with top 5 memes displayed.
+"""
+
+import tkinter as tk
+# import PIL
 
 
-window.mainloop()
+def memebot_display():
+    window = tk.Tk()
+    frame = tk.Frame(window)
+    window.geometry('900x600')
+    frame.pack()
+
+    memebot_text = "MemeBot's Dank Memes"
+    floating_text = tk.Label(window, text=memebot_text)
+    floating_text.pack(fill='both', expand='yes')
+    floating_text.place(x=375, y=5)
+
+
+    blue_meme = tk.PhotoImage(file="gui_mock_imgs/bluememe.gif")
+    blue_meme_as_button = tk.Button(frame, compound=tk.TOP, width=375, height=410, image=blue_meme,
+                        text="That's a Dank blue Meme!!", bg='light blue')
+    blue_meme_as_button.pack(side=tk.LEFT, padx=25, pady=40)
+    blue_meme_as_button.image = blue_meme
+
+
+    blue_button = tk.Button(window, text='Blue meme is so Dank!!', fg='blue')
+    blue_button.pack(side='left', expand='no')
+
+
+    red_meme = tk.PhotoImage(file="gui_mock_imgs/redmeme.gif")
+    red_meme_as_button = tk.Button(frame, compound=tk.TOP, width=375, height=410, image=red_meme,
+                        text="That's a Dank red Meme!", bg='pink')
+    red_meme_as_button.pack(side=tk.LEFT, padx=25, pady=40)
+    red_meme_as_button.image = red_meme
+    red_button = tk.Button(window, text='Red meme is so Dank!', fg='red')
+    red_button.pack(side='right', expand='no')
+
+    window.mainloop()
+
+def main():
+    memebot_display()
+
+main()
