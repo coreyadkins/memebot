589b91fa6ddb53f357dfe6bf149c561f86a72afb
Corey Adkins <adkinsbass@gmail.com>
2016-08-01 12:22:31 -0700

    Completes piping of Dank or Stank functions into main

--- gui.py
+++ gui.py
@@ -3,7 +3,8 @@ that collect a vote for the meme clicked and close the window when voted on.
 """
 
 import tkinter as tk
-import vote
+from PIL import ImageTk
+from vote.vote import MemeVote
 import PIL
 
 class DankOrStank:
@@ -13,6 +14,7 @@ class DankOrStank:
         self.master = master
         self.meme_1 = meme_1
         self.meme_2 = meme_2
+        self.winner = None
         frame = tk.Frame(master)
         master.title("Memebot")
         master.geometry('900x600')
@@ -21,8 +23,7 @@ class DankOrStank:
         self.label = tk.Label(master, text="Dank or Stank", font=('Times', 40))
         self.label.pack()
 
-        left_image = PIL.ImageTk.PhotoImage(PIL.Image.open(meme_1))
-        self.left_meme = tk.PhotoImage(file=left_image)
+        self.left_meme =  PIL.ImageTk.PhotoImage(PIL.Image.open(meme_1))
         self.left_meme_as_button = tk.Button(
             frame,
             compound=tk.TOP,
@@ -35,8 +36,7 @@ class DankOrStank:
         self.left_meme_as_button.pack(side='left', padx=25, pady=40)
         self.left_meme_as_button.image = self.left_meme
 
-        right_image = PIL.ImageTk.PhotoImage(PIL.Image.open(meme_2))
-        self.right_meme = tk.PhotoImage(file=right_image)
+        self.right_meme = PIL.ImageTk.PhotoImage(PIL.Image.open(meme_2))
         self.right_meme_as_button = tk.Button(
             frame,
             compound=tk.TOP,
@@ -51,10 +51,12 @@ class DankOrStank:
 
     def vote_meme_1(self):
         """Returns a Vote Object for meme_1"""
-        return vote.MemeVote([self.meme_1], [self.meme_2], [self.meme_1])
+        self.winner = self.meme_1
         self.master.quit()
 
     def vote_meme_2(self):
         """Returns a Vote Object for meme_2"""
-        return vote.MemeVote([self.meme_1], [self.meme_2], [self.meme_2])
+        self.winner = self.meme_2
         self.master.quit()
+
+
--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 """Main."""
 
-from text_generator import generate_text
-from image_writer import write_text_to_image
-from PIL import Image
-from random import randint
-from os import path, makedirs
-from randomfile import get_random_input_file, get_random_output_path
+from randomfile import get_random_input_file
+from gui import DankOrStank
+import tkinter as tk
+from vote.vote import MemeVote
 
+def run_gui(meme_1, meme_2):
+    """Runs the Stank or Dank gui for memebot"""
+    root = tk.Tk()
+    meme_or_not = DankOrStank(root, meme_1, meme_2)
+    root.mainloop()
+    return meme_or_not.winner
 
-def save_image(image, image_name):
-    file_name = str(image_name).replace('input/', '')
-    output_name = get_random_output_path(file_name)
-    if not path.exists('output'):
-        makedirs('output')
-    image.save(output_name)
-
-
-def main():
-    top_line, bottom_line = generate_text()
-    image_name = get_random_input_file()
-    image = write_text_to_image(top_line, bottom_line, image_name)
-    save_image(image, image_name)
-
+def main ():
+    image_1 = get_random_input_file(dir='output')
+    image_2 = get_random_input_file(dir='output')
+    winner = run_gui(image_1, image_2)
+    MemeVote().vote(image_1, image_2, winner)
 
 if __name__ == '__main__':
     main()
